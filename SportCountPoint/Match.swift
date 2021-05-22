5682e6052478377675036b7de6ebaf3ec31a18be
Adri3n89 <adrien@perea.be>
2021-05-22 21:42:26 +0200

    V1.1

--- SportCountPoint/Match.swift
+++ SportCountPoint/Match.swift
@@ -16,28 +16,4 @@ class Match {
     var isOver: Bool {
         return winner != nil
     }
-
-    var scores: [Player: Int] {
-        return getScores()
-    }
-
-    private var currentSet: Set {
-        return sets.last!
-    }
-
-    static let maxNumberOfWonSets = 2
-
-    // MARK: - Private Getters
-    
-    private func getScores() -> [Player: Int] {
-        var scores = [Player.one: 0, Player.two: 0]
-
-        for set in sets {
-            if let setWinner = set.winner {
-                scores[setWinner]! += 1
-            }
-        }
-        return scores
-    }
-
 }
